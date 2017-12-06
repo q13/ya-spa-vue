367dbdcf88b2470644bfb729bfc5f9a7bd2f658a
q13 <qsimon13@gmail.com>
2017-12-06 13:51:34 +0800

    package.json name改成占位符

--- src/app/index.js
+++ src/app/index.js
@@ -9,18 +9,13 @@ import {
 
 // 数据挂钩
 hook('prepare@app', function () {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      const appData = {
-      };
-      resolve(appData);
-    }, 0);
-  });
+  return { a: 1 };
 });
 // App component挂钩，将component configs传递给resolve完成初始化
 hook('app@app', function (options) {
   const resolve = options.resolve;
   const componentOptions = options.componentOptions;
+  console.log('appData', options.appData);
   // 设置document title
   setDocumentTitle('Test');
   setTimeout(() => {
