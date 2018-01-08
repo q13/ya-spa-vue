19962431e906fb835b3ee78b6ded5f37b0eab24e
q13 <qsimon13@gmail.com>
2018-01-08 17:33:24 +0800

    vue引用来源统一

--- tools/webpack.base.conf.js
+++ tools/webpack.base.conf.js
@@ -20,7 +20,8 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.vue', '.json'],
     alias: {
-      'vue$': 'vue/dist/vue.esm.js',
+      // 'vue$': 'vue/dist/vue.esm.js',
+      'vue$': resolve('node_modules/vue/dist/vue.esm.js'),
       '@': resolve('src'),
       '+': resolve('ya'),
       'ya-ui-vue': resolve('node_modules/ya-ui-vue')
