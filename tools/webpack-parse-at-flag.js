258507dae7c2da49ff9fe660244d006f6e6c0efd
q13 <qsimon13@gmail.com>
2018-01-22 14:54:51 +0800

    兼容mac系统

--- tools/webpack-parse-at-flag.js
+++ tools/webpack-parse-at-flag.js
@@ -1,6 +1,8 @@
 /**
  * 解析node_modules下@引用问题，从自身包位置src目录作为基准替换
  */
+const path = require('path');
+
 function ParseAtPlugin() {
 }
 
@@ -9,7 +11,8 @@ ParseAtPlugin.prototype.apply = function(compiler) {
     compilation.plugin("before-resolve", function (result, callback) {
       let { context, request,  dependencies} = result;
       if (request.slice(0, 2) === '@/') {
-        context = context.split('\\');
+        // context = context.split('\\');
+        context = context.split(path.sep);
         let baseIndex = -1;
         // 默认从第一个遇到的src位置处替换，有较小概率遇到替换不正确现象（src重名）
         context.some((temp, index) => {
@@ -21,7 +24,8 @@ ParseAtPlugin.prototype.apply = function(compiler) {
         if (baseIndex >= 0) {
           let basePathArr = context.slice(0, baseIndex + 1);
           let fullPathArr = basePathArr.concat(request.slice(2).split('/'));
-          result.request = fullPathArr.join('\\');
+          // result.request = fullPathArr.join('\\');
+          result.request = fullPathArr.join(path.sep);
         }
       }
       callback(null, result);
