42d1b663efc26e90a1edb9149f8a001339e89e1d
q13 <qsimon13@gmail.com>
2018-02-06 17:20:37 +0800

    node install不输出安装日志

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ npm config set registry https://registry.npm.taobao.org;
 rm -rf node_modules/ipos-zeus*;
 rm -rf node_modules/air-chain*;
 rm -rf node_modules/ya-ui-vue;
-npm install --unsafe-perm;
+npm install --unsafe-perm --quiet;
 node tools/build.js --app-env=$1 --app-name=$2 --api-domain=$3;
