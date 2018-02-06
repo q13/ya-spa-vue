22c4075b083ddef1f7cd007bb46e6c9658e0c82c
q13 <qsimon13@gmail.com>
2018-02-06 17:31:35 +0800

    抑制npm install安装进度

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ npm config set registry https://registry.npm.taobao.org;
 rm -rf node_modules/ipos-zeus*;
 rm -rf node_modules/air-chain*;
 rm -rf node_modules/ya-ui-vue;
-npm install --unsafe-perm --quiet;
+npm install --unsafe-perm --quiet --no-progress;
 node tools/build.js --app-env=$1 --app-name=$2 --api-domain=$3;
