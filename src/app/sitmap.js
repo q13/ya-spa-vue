b424c3306dab5400ec8940ac053ffd37ad68cf56
q13 <qsimon13@gmail.com>
2018-07-16 18:35:33 +0800

    lodash modularized

--- src/app/sitmap.js
+++ src/app/sitmap.js
@@ -12,5 +12,15 @@ export default function () {
       path: '/nil/:props?',
       component: () => import('../pages/nil/index')
     }
+  }, {
+    isCache: false, // 是否被缓存
+    navText: '', // 导航文字
+    navLink: '#', // 导航链接
+    navVisible: false, // 导航是否可见
+    permission: 'all', // 权限控制，无具备权限将会被筛选掉
+    route: {
+      path: '/',
+      component: () => import('../pages/welcome/index')
+    }
   }];
 };
---
+++ src/pages/welcome/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="welcome-body">
+    Hey guy! Here is A SPA project framework.
+  </div>
+</template>
+
+<script>
+export default {
+  created() {
+    console.log('Welcome created!');
+  }
+}
+</script>
+
+<style>
+
+</style>
